b357db07584a209fc3ca08ba07221024d763b7da
Pr-ElTech <empero.lehnzy42@gmail.com>
2026-03-04 19:21:55 +0100

    added signup and login handle function

--- src/components/SignUpBox.jsx
+++ src/components/SignUpBox.jsx
@@ -10,18 +10,22 @@ const [signupData,setSignupData] = useState ({
 })
 
 const handleSingUp = () =>{
+  if (!signupData.Email ||!signupData.PhoneNumber|| !signupData.Password ){
+    alert ("all fields required!")
+  }
+
 
 }
 
   return (
     <div>
-
-        <input type="email"  placeholder='Email' value={signupData.Email} />
-        <input type="number"  placeholder='Phone Number' value={signupData.PhoneNumber} />
-        <input type="text"  placeholder='Username' value={signupData.UserName} />
-        <input type="text"   placeholder='Password' value={signupData.Password}/>
-        <input type="text"   placeholder='Confirm Password'/>
-        <button>SignUp</button>
+   
+        <input type="email"  placeholder='Email' value={signupData.Email} onChange={(e)=>setSignupData({...signupData, Email:e.target.value})}/>
+        <input type="number"  placeholder='Phone Number' value={signupData.PhoneNumber} onChange={(e)=>setSignupData({...signupData, PhoneNumber:e.target.value})} />
+        <input type="text"  placeholder='Username' value={signupData.UserName} onChange={(e)=>setSignupData({...signupData, UserName:e.target.value})} />
+        <input type="text"   placeholder='Password' value={signupData.Password} onChange={(e)=>setSignupData({...signupData, Password:e.target.value})}/>
+        <input type="text"   placeholder='Confirm Password' value={signupData.Email} onChange={(e)=>setSignupData({...signupData, Email:e.target.value})}/>
+        <button onClick={handleSingUp}>SignUp</button>
     </div>
   )
 }
