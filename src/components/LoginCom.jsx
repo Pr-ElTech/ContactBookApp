8ec71a491868851f570da8122671c2ae43e44601
Pr-ElTech <empero.lehnzy42@gmail.com>
2026-03-06 07:00:10 +0100

    style/ login & Signup component and worked on handlesignup function reamining Json

--- src/components/LoginCom.jsx
+++ src/components/LoginCom.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import "../CSS/LogiCom.css"
 
 const LoginCom = () => {
 const [loginData,setLoginData] = useState ({
@@ -22,7 +23,7 @@ else{
 }
 
   return (
-    <div>
+    <div className='loginbox'>
         <input type="email"  placeholder='Email / username' value={loginData.Email || loginData.UserName }  onChange={(e)=>set({...input, UserName:e.target.value? Email:e.target.value})}/>
         <input type="text"   placeholder='Password' value={loginData.Password} onChange={(e)=>set({...input, Password:e.target.value})}/>
         <button onClick={handleloginData}>Login</button>
--- src/components/SignUpBox.jsx
+++ src/components/SignUpBox.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import "../CSS/Signup.css"
 
 const SignUpBox = () => {
 
@@ -6,25 +7,43 @@ const [signupData,setSignupData] = useState ({
     Email:"",
     PhoneNumber:"",
     UserName:"",
-    Password:""
+    Password:"",
+    confirmPass: ""
 })
+
+const [userDataBox, setUserDataBox] = useState ([])
 
 const handleSingUp = () =>{
   if (!signupData.Email ||!signupData.PhoneNumber|| !signupData.Password ){
     alert ("all fields required!")
+    return false;
+  }
+  else if(signupData.Password.length < 8){
+    alert("password not strong enough")
+    return false;
+  }
+  else if(signupData.Password !== signupData.confirmPass){
+    alert("password mismatch")
+    return false;
+  }
+
+  else{
+const sucessfulData =  [...userDataBox, signupData]
+setUserDataBox (sucessfulData)
+console.log(sucessfulData)
   }
 
 
 }
 
   return (
-    <div>
+    <div className='signupbox'>
    
         <input type="email"  placeholder='Email' value={signupData.Email} onChange={(e)=>setSignupData({...signupData, Email:e.target.value})}/>
         <input type="number"  placeholder='Phone Number' value={signupData.PhoneNumber} onChange={(e)=>setSignupData({...signupData, PhoneNumber:e.target.value})} />
         <input type="text"  placeholder='Username' value={signupData.UserName} onChange={(e)=>setSignupData({...signupData, UserName:e.target.value})} />
-        <input type="text"   placeholder='Password' value={signupData.Password} onChange={(e)=>setSignupData({...signupData, Password:e.target.value})}/>
-        <input type="text"   placeholder='Confirm Password' value={signupData.Email} onChange={(e)=>setSignupData({...signupData, Email:e.target.value})}/>
+        <input type="password"   placeholder='Password' value={signupData.Password} onChange={(e)=>setSignupData({...signupData, Password:e.target.value})}/>
+        <input type="password"   placeholder='Confirm Password' value={signupData.confirmPass} onChange={(e)=>setSignupData({...signupData, confirmPass:e.target.value})}/>
         <button onClick={handleSingUp}>SignUp</button>
     </div>
   )
