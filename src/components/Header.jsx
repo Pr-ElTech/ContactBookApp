077f5237ecfbcb08780f858fc1f75306203854ea
Pr-ElTech <empero.lehnzy42@gmail.com>
2026-03-14 00:27:51 +0100

    debuging

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,54 @@
-import React from 'react'
-import '../CSS/Header.css'
-const Header = ({users}) => {
+// import React from 'react'
+// import '../CSS/Header.css'
+// const Header = ({users}) => {
+//   return (
+// <header>
+//     <div className='logo'><h1>PR-EL <span>TECH</span>/C</h1></div>
+
+//     <div className='userpart'>{users? ({users.?map((item)=>(<h2 key={item.ID }>{item?.userName?.charAt(0).toUpperCase()}</h2>))}
+//       && <button>Logout</button> ): (<button>Login / Signin</button>
+
+//       && <button>Signup / Register</button>)
+
+//       }
+//     </div>
+
+// </header>
+//   )
+// }
+
+// export default Header
+
+import React from "react";
+import "../CSS/Header.css";
+
+const Header = ({ users }) => {
   return (
-<header>
-    <div className='logo'><h1>PR-EL <span>TECH</span>/C</h1></div>
+    <header>
+      <div className="logo">
+        <h1>
+          PR-EL <span>TECH</span>/C
+        </h1>
+      </div>
 
-    <div className='userpart'>
+      <div className="userpart">
+        {/* Check if users exists and has items */}
+        {users && users.length > 0 ? (
+          <>
+            {users.map((item) => (
+              <h2 key={item.ID}>{item?.userName?.charAt(0).toUpperCase()}</h2>
+            ))}
+            <button>Logout</button>
+          </>
+        ) : (
+          <>
+            <button>Login / Signin</button>
+            <button>Signup / Register</button>
+          </>
+        )}
+      </div>
+    </header>
+  );
+};
 
-      {users.map((item)=>(<h2 key={item.ID }>{item?.userName?.charAt(0).toUpperCase()}</h2>))}  
-      <button>Login / Signin</button>
-      <button>Logout</button>
-      <button>Signup / Register</button>
-    </div>
-
-
-</header>
-  )
-}
-
-export default Header
+export default Header;
